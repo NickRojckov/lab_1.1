9d4b7fb67ea443f8484d99319f5a649410f95f6d
NickRojckov <43681609+NickRojckov@users.noreply.github.com>
2019-11-15 17:18:10 +0200

    Add files via upload

---
+++ задание 1.py
@@ -0,0 +1,18 @@
+a = int(input())
+b = int(input())
+c = int(input())
+X = 0
+Y = 0
+if a>0:
+    X+=1
+elif a<0:
+    Y+=1
+if b>0:
+    X+=1
+elif b<0:
+    Y+=1
+if c>0:
+    X+=1
+elif c<0:
+    Y+=1
+print(X,Y)
